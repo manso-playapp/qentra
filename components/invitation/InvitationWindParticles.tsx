795a3bc86cb5608972db3e63a3351969ac75b0ed
Javier Manso <grupomanso@gmail.com>
2026-07-23 10:28:50 -0300

    Increase invitation particle density

--- components/invitation/InvitationWindParticles.tsx
+++ components/invitation/InvitationWindParticles.tsx
@@ -24,6 +24,19 @@ const PARTICLES: Particle[] = [
   { top: 31, left: 18, size: 4, duration: 24, delay: -20, opacity: 0.26 },
   { top: 52, left: 45, size: 2, duration: 17, delay: -2, opacity: 0.55 },
   { top: 93, left: 52, size: 3, duration: 26, delay: -15, opacity: 0.36 },
+  { top: 3, left: 36, size: 1, duration: 21, delay: -13, opacity: 0.46 },
+  { top: 16, left: 63, size: 6, duration: 30, delay: -27, opacity: 0.22 },
+  { top: 23, left: 3, size: 2, duration: 16, delay: -6, opacity: 0.56 },
+  { top: 38, left: 56, size: 5, duration: 25, delay: -18, opacity: 0.27 },
+  { top: 45, left: 96, size: 1, duration: 20, delay: -4, opacity: 0.52 },
+  { top: 59, left: 22, size: 6, duration: 31, delay: -23, opacity: 0.2 },
+  { top: 67, left: 80, size: 2, duration: 17, delay: -10, opacity: 0.5 },
+  { top: 75, left: 33, size: 5, duration: 29, delay: -25, opacity: 0.24 },
+  { top: 82, left: 91, size: 1, duration: 22, delay: -9, opacity: 0.58 },
+  { top: 97, left: 16, size: 4, duration: 24, delay: -16, opacity: 0.3 },
+  { top: 29, left: 69, size: 1, duration: 18, delay: -1, opacity: 0.55 },
+  { top: 54, left: 5, size: 5, duration: 28, delay: -22, opacity: 0.23 },
+  { top: 89, left: 64, size: 2, duration: 19, delay: -12, opacity: 0.47 },
 ]
 
 export default function InvitationWindParticles() {
